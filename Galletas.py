d9884907397c4cbb214c23b220ae435f9b55f655
Washaaaan <ltpalangornall@gmail.com>
2023-04-10 18:23:46 -0400

    Agregué una línea de código en Galletas.py que indica que fue modificado el archivo

--- Galletas.py
+++ Galletas.py
@@ -5,5 +5,4 @@ total_personas = invitados + 1 # Se le suma 1 a la variable ya que la persona ta
 galletas_recibidas = total_galletas // total_personas # Se calcula cuántas galletas recibirá cada persona mediante división de parte entera.
 galletas_sobrantes = total_galletas % total_personas # Se calcula cuántas galletas sobrarán mediante el módulo.
 print(f"En total son {total_personas} personas. Cada una recibe {galletas_recibidas} galletas y sobran {galletas_sobrantes}.")
-
-
+print("Hola, modifiqué el archivo")
